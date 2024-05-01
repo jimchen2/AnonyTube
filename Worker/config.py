82286fef3c19e17b95617c562a44833d9d3a2ac1
user <jimchen4214@gmail.com>
2024-05-02 01:20:53 +0800

    updates

--- Worker/config.py
+++ Worker/config.py
@@ -1,7 +1,7 @@
 # config.py
 
 # MongoDB configuration
-mongoURI = "mongodb://localhost:27017/test" 
+mongoURI = "mongodb://localhost:27017/anonytube" 
 
 # S3 configuration
 publicurl = "https://cloudflare.jimchen.me"
@@ -10,4 +10,4 @@ s3_access_key = ""
 s3_secret_key = ""
 s3_endpoint = "https://ee64074f404e392345763d53572d3d2e.r2.cloudflarestorage.com"
 local_model_dir = "/home/builduser/nllb-200-distilled-600M"
-database_name="test"
+database_name="anonytube"
--- setup.sh
+++ setup.sh
@@ -1,7 +1,24 @@
 # add /config.js  
+###########################################################################################
 
 sudo pacman -Syu --noconfirm; sudo pacman -S git base-devel npm nodejs nginx certbot --noconfirm
 sudo useradd -m builduser && sudo passwd -d builduser && echo 'builduser ALL=(ALL) NOPASSWD: ALL' | sudo tee /etc/sudoers.d/builduser
 sudo -u builduser bash -c 'cd ~; git clone https://aur.archlinux.org/yay-bin.git; cd yay-bin; makepkg -si --noconfirm; cd ..; rm -rf yay-bin; yay -S mongodb-tools mongosh-bin mongodb-bin --noconfirm'
-sudo mkdir -p /var/www; sudo git clone https://github.com/jimchen2/AnonyTube-updated /var/www/My-Website; sudo chown -R builduser:builduser /var/www/My-Website
-sudo mv /config.js 
+sudo systemctl enable --now mongodb
+###########################################################################################
+# Only this part below in the (same) system
+sudo mkdir -p /var/www; sudo git clone https://github.com/jimchen2/AnonyTube /var/www/AnonyTube; sudo chown -R builduser:builduser /var/www/AnonyTube
+sudo -u builduser bash -c 'cd /var/www/AnonyTube; mongorestore --dir=./dump;'
+
+sudo -u builduser bash -c 'cd /var/www/AnonyTube/VideoPlatform; npm install'
+sudo cp /var/www/AnonyTube/VideoPlatform.service /etc/systemd/system/VideoPlatform.service && sudo mv /config.js /var/www/AnonyTube/VideoPlatform/config.js
+sudo systemctl daemon-reload && sudo systemctl enable --now VideoPlatform
+
+sudo -u builduser bash -c 'cd /var/www/AnonyTube/frontend; npm install; npm run build'
+
+certbot certonly --standalone -d anonytube.jimchen.me -d tubeapp.org --email jimchen4214@gmail.com --non-interactive --agree-tos && systemctl enable --now certbot-renew.timer 
+
+mkdir -p /etc/nginx/{sites-available,sites-enabled} && sudo ln -sf /etc/nginx/sites-available/AnonyTube.conf /etc/nginx/sites-enabled/
+sudo cp /var/www/AnonyTube/AnonyTube.conf /etc/nginx/sites-available/AnonyTube.conf
+sudo cp /var/www/AnonyTube/nginx.conf /etc/nginx/nginx.conf
+sudo systemctl enable --now nginx
